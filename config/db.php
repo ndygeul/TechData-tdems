8fb632c071e2889195bc2fc00041a689e8481469
ndygeul <auaoaoau@gmail.com>
2025-08-23 09:44:48 +0900

    수정

--- config/db.php
+++ config/db.php
@@ -1,10 +1,11 @@
 <?php
-$host = "172.17.0.4:30701";
+$host = "172.17.0.4";
+$port = 30701;
 $user = "ezk";
 $pass = "dlwlzpdl";
 $dbname = "tdems";
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
 if ($conn->connect_error) {
     die("DB 연결 실패: " . $conn->connect_error);
 }
