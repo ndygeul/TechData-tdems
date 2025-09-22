8efdfe7a1f3fa344416c787730e93f33a2a0682a
ndygeul <auaoaoau@gmail.com>
2025-09-22 14:06:46 +0900

    수정

--- config/db.php
+++ config/db.php
@@ -1,13 +1,12 @@
 <?php
 $host = "";
-$port = "";
 $user = "";
 $pass = "";
 $dbname = "";
 
 $conn = null;
 if ($host !== "" && $user !== "" && $dbname !== "") {
-    $conn = @new mysqli($host, $user, $pass, $dbname, $port);
+    $conn = @new mysqli($host, $user, $pass, $dbname);
     if ($conn->connect_error) {
         $conn = null;
     } else {
